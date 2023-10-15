80ceb5291cf35a8b9953534f7ae26412a21d6b3b
VeniVeci <1453331342@qq.com>
2023-10-15 15:15:06 +0800

    原型模式：java中就是clone ，一种创建型模式

--- src/com/atguigu/prototype/deepclone/Client.java
+++ src/com/atguigu/prototype/deepclone/Client.java
@@ -7,20 +7,20 @@ public class Client {
 		DeepProtoType p = new DeepProtoType();
 		p.name = "宋江";
 		p.deepCloneableTarget = new DeepCloneableTarget("大牛", "小牛");
-		
+
 		//方式1 完成深拷贝
-		
-//		DeepProtoType p2 = (DeepProtoType) p.clone();
-//		
-//		System.out.println("p.name=" + p.name + "p.deepCloneableTarget=" + p.deepCloneableTarget.hashCode());
-//		System.out.println("p2.name=" + p.name + "p2.deepCloneableTarget=" + p2.deepCloneableTarget.hashCode());
-	
-		//方式2 完成深拷贝
-		DeepProtoType p2 = (DeepProtoType) p.deepClone();
-		
+
+		DeepProtoType p2 = (DeepProtoType) p.clone();
+
 		System.out.println("p.name=" + p.name + "p.deepCloneableTarget=" + p.deepCloneableTarget.hashCode());
 		System.out.println("p2.name=" + p.name + "p2.deepCloneableTarget=" + p2.deepCloneableTarget.hashCode());
-	
+
+		//方式2 完成深拷贝
+//		DeepProtoType p2 = (DeepProtoType) p.deepClone();
+//
+//		System.out.println("p.name=" + p.name + "p.deepCloneableTarget=" + p.deepCloneableTarget.hashCode());
+//		System.out.println("p2.name=" + p.name + "p2.deepCloneableTarget=" + p2.deepCloneableTarget.hashCode());
+
 	}
 
 }
