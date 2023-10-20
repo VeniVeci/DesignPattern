25e75698c2e95ef1fc82ee2dd2503194d86a3faa
VeniVeci <1453331342@qq.com>
2023-10-20 21:57:03 +0800

    装饰器模式添加StrongStu

--- src/com/MyDemo/decorator/Demo.java
+++ src/com/MyDemo/decorator/Demo.java
@@ -1,7 +1,10 @@
 package com.MyDemo.decorator;
 
 import java.lang.management.ThreadInfo;
-
+/*
+用来增强原有类： 有点像面向切面编程来进行原有功能的增强。
+通过依赖该对象
+ */
 /**
  * @author VeniVeci
  * @program: DesignPattern
@@ -13,8 +16,10 @@ public class Demo {
     public static void main(String[] args) {
         Person person = new Student("kiki");
         person.opr();
-        person = new DecoratorA(person);
-        person.opr();
+        Person person1 = new DecoratorA(person);
+        person1.opr();
+        Person person2 = new StrongStu(person);
+        person2.opr();
     }
 
 }
@@ -43,7 +48,18 @@ class DecoratorA extends Decorator{
 
     @Override
     public void opr() {
-        person.opr();
+//        person.opr();
         System.out.println(person.name + " study plus");
+    }
+}
+class StrongStu extends Decorator{
+    public StrongStu(Person person){
+        this.person = person;
+    }
+
+    @Override
+    public void opr() {
+//        person.opr();
+        System.out.println(person.name + " is a strong student");
     }
 }
